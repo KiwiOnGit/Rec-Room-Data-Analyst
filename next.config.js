853abf6c494d6de2a8ec0967456ab668e7984560
Ely Wright <169009311+KiwiOnGit@users.noreply.github.com>
2026-04-10 15:44:26 -0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',          // static export for GitHub Pages / Vercel
-  images: { unoptimized: true },
-  trailingSlash: true,
-};
-module.exports = nextConfig;
